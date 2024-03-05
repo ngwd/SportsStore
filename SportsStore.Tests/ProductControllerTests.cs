cf0df5bd0a268e54973e976b2ee1a36e837f4b1d
ngwd <ngwuedag@gmail.com>
2024-03-05 12:58:22 -0800

    Add pagination to the product controller and add the unit test for it

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using SportsStore.Models;
+using SportsStore.Controllers;
+
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Pagination()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock
+                .Setup(m => m.Products)
+                .Returns((new Product[] {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"},
+                    new Product {ProductID = 4, Name = "P4"},
+                    new Product {ProductID = 5, Name = "P5"}
+                })
+                .AsQueryable<Product>());
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // act
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>; 
+
+            // assert 
+            Product[] prod = result.ToArray();
+            Assert.True(2 == prod.Length);
+            Assert.Equal("P4", prod[0].Name);
+            Assert.Equal("P5", prod[1].Name);
+        }
+    }
+}
--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
-
+using System.Linq;
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public IActionResult Index()
         {
@@ -15,6 +16,11 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
-        public ViewResult List() => View(repository.Products);
+        // public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1) => 
+            View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage-1)*PageSize)
+                .Take(PageSize));
     }
 }
